ca82384615ea1489bbbee81f24d5780ad005a80f
tranngan97 <tranngana5@gmail.com>
2019-04-23 23:21:28 +0700

    .

---
+++ js/cv.js
@@ -0,0 +1,20 @@
+$(function(){
+
+	// $(".skill_ele").each(function(){
+	// 	$(this).click(function(){
+	// 	$(this).children(".sk_de").toggleClass("active");
+	// 	$(this).parent().next().slideDown();
+
+	// });
+	// })
+	// $(".my_edu li").each(function(){
+	// 	$(this).click(function(){
+	// 		$(this).children(".detail").css("display", "block");
+	// 	})
+	// });
+
+
+
+
+
+})
